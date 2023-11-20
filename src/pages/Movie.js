0c987096904e91fb5b33b59b19c6a6cd472b3336
jeffmanhart <jeffreymanhart@gmail.com>
2023-11-20 11:40:55 -0700

    Did all the routing things

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movieDetails, setMovieDetails] = useState({})
+  const params = useParams();
+  const movieId = params.id
+
+  useEffect(() =>{
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then(r => r.json())
+    .then(data => setMovieDetails(data))
+    .catch(error => console.error(error))
+  }, [movieId])
+  
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movieDetails.title}</h1>
+        <p>{movieDetails.time}</p>
+        <span>{movieDetails.genres}</span>
       </main>
     </>
   );
